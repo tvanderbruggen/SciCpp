5f555eb1527d7cc1a4a93de8731516ccbc320468
thomas <th.vanderbruggen@gmail.com>
2019-01-16 00:00:37 +0100

    Add benchmarks support

---
+++ benchmarks/scicpp_benchmark.cpp
@@ -0,0 +1,12 @@
+// clang-format off
+#define NONIUS_RUNNER
+#include "nonius.h++"
+// clang-format on
+
+// #include <scicpp/numeric.b.cpp>
+// #include <scicpp/random.b.cpp>
+// #include <scicpp/signal/fft.b.cpp>
+#include <scicpp/signal/convolve.b.cpp>
+// #include <scicpp/signal/windows.b.cpp>
+// #include <scicpp/signal/convolve.b.cpp>
+// #include <scicpp/polynomials/polynomial.b.cpp>
