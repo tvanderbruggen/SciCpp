6c9ecf2ed2a09619362a9b0473d4610495180fd6
thomas <th.vanderbruggen@gmail.com>
2019-01-21 00:34:39 +0100

    Clean up fabs

--- scicpp/core/maths.hpp
+++ scicpp/core/maths.hpp
@@ -2,6 +2,7 @@
 #define SCICPP_CORE_MATHS
 
 #include "scicpp/core/functional.hpp"
+#include "scicpp/core/meta.hpp"
 
 #include <array>
 #include <cmath>
@@ -15,45 +16,24 @@ namespace scicpp {
 // fabs
 //---------------------------------------------------------------------------------
 
-// https://codereview.stackexchange.com/questions/60140/generic-absolute-value-function
-
 template <typename T>
-constexpr T fabs(const T &x) {
-    static_assert(std::is_floating_point<T>::value);
-
-// Handles negative zero
-// Could use std::fpclassify(x) == FP_ZERO
-// to quiet warning, but this is not constexpr.
+constexpr auto fabs(T &&x) {
+    if constexpr (meta::is_iterable_v<T>) {
+        return map([&](auto v) { return fabs(v); }, std::forward<T>(x));
+    } else {
+        // Handles negative zero
+        // https://codereview.stackexchange.com/questions/60140/generic-absolute-value-function
+        // Could use std::fpclassify(x) == FP_ZERO
+        // to quiet warning, but this is not constexpr.
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wfloat-equal"
-    if (x == 0.) {
-        return +0.;
-    }
+        if (x == 0.) {
+            return +0.;
+        }
 #pragma GCC diagnostic pop
 
-    return (x < 0.) ? -x : x;
-}
-
-template <typename T, std::size_t N>
-auto fabs(const std::array<T, N> &a) {
-    std::array<T, N> res;
-
-    for (std::size_t i = 0; i < N; ++i) {
-        res[i] = fabs(a[i]);
+        return (x < 0.) ? -x : x;
     }
-
-    return res;
-}
-
-template <typename T>
-auto fabs(const std::vector<T> &v) {
-    std::vector<T> res(v.size());
-
-    for (std::size_t i = 0; i < v.size(); ++i) {
-        res[i] = fabs(v[i]);
-    }
-
-    return res;
 }
 
 //---------------------------------------------------------------------------------
