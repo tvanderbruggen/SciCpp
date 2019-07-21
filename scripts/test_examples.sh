752912309c17a57123412a9429d6256aebb765fd
thomas <th.vanderbruggen@gmail.com>
2019-07-21 16:26:45 +0200

    Add wave-height to example tests

--- scripts/test_examples.sh
+++ scripts/test_examples.sh
@@ -11,6 +11,7 @@ make COMPILER=$compiler EXAMPLE=estimate-zeros example_py
 make COMPILER=$compiler EXAMPLE=white-noise example
 make COMPILER=$compiler EXAMPLE=white-noise example_py
 make COMPILER=$compiler EXAMPLE=load-text-data example
+make COMPILER=$compiler EXAMPLE=wave-height example
 
 # doc examples
 
