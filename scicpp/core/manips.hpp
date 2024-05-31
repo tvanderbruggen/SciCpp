d2f832220317f71fe4548721c297c69887d1279b
Thomas Vanderbruggen <th.vanderbruggen@gmail.com>
2024-05-31 23:07:39 +0200

    concatenate quantities

--- scicpp/core/manips.hpp
+++ scicpp/core/manips.hpp
@@ -9,6 +9,7 @@
 #include "scicpp/core/macros.hpp"
 #include "scicpp/core/meta.hpp"
 #include "scicpp/core/range.hpp"
+#include "scicpp/core/units/quantity.hpp"
 
 #include <array>
 #include <iterator>
@@ -21,23 +22,25 @@ namespace scicpp {
 // concatenate
 //-----------------------------------------------------------------------------
 
-namespace detail {
-
-template <typename Array1, typename Array2, typename ArrayOut>
-void concatenate_impl(const Array1 &a1, const Array2 &a2, ArrayOut &res) {
-    std::copy(a1.cbegin(), a1.cend(), res.begin());
-    std::copy(a2.cbegin(), a2.cend(), res.begin() + signed_size_t(a1.size()));
-}
-
-} // namespace detail
-
 template <typename T1, std::size_t N1, typename T2, std::size_t N2>
 auto concatenate(const std::array<T1, N1> &a1, const std::array<T2, N2> &a2) {
-    using T = std::common_type_t<T1, T2>;
+    if constexpr (units::is_quantity_v<T1>) {
+        static_assert(units::is_same_dimension<T1, T2>);
 
-    std::array<T, N1 + N2> res{};
-    detail::concatenate_impl(a1, a2, res);
-    return res;
+        std::array<T1, N1 + N2> res{};
+        std::copy(a1.cbegin(), a1.cend(), res.begin());
+        std::transform(a2.cbegin(),
+                       a2.cend(),
+                       res.begin() + signed_size_t(a1.size()),
+                       [](auto x) { return units::quantity_cast<T1>(x); });
+        return res;
+    } else {
+        std::array<std::common_type_t<T1, T2>, N1 + N2> res{};
+        std::copy(a1.cbegin(), a1.cend(), res.begin());
+        std::copy(
+            a2.cbegin(), a2.cend(), res.begin() + signed_size_t(a1.size()));
+        return res;
+    }
 }
 
 template <typename Array1,
@@ -47,11 +50,24 @@ template <typename Array1,
 auto concatenate(const Array1 &a1, const Array2 &a2) {
     using T1 = typename Array1::value_type;
     using T2 = typename Array2::value_type;
-    using T = std::common_type_t<T1, T2>;
 
-    std::vector<T> res(a1.size() + a2.size());
-    detail::concatenate_impl(a1, a2, res);
-    return res;
+    if constexpr (units::is_quantity_v<T1>) {
+        static_assert(units::is_same_dimension<T1, T2>);
+
+        std::vector<T1> res(a1.cbegin(), a1.cend());
+        res.resize(a1.size() + a2.size());
+        std::transform(a2.cbegin(),
+                       a2.cend(),
+                       res.begin() + signed_size_t(a1.size()),
+                       [](auto x) { return units::quantity_cast<T1>(x); });
+        return res;
+    } else {
+        std::vector<std::common_type_t<T1, T2>> res(a1.cbegin(), a1.cend());
+        res.resize(a1.size() + a2.size());
+        std::copy(
+            a2.cbegin(), a2.cend(), res.begin() + signed_size_t(a1.size()));
+        return res;
+    }
 }
 
 template <typename Array, typename T, meta::enable_if_iterable<Array> = 0>
@@ -61,7 +77,14 @@ auto concatenate(std::vector<T> &&a1, const Array &a2) {
     const auto N1 = a1.size();
     a1.resize(N1 + a2.size());
 
-    if constexpr (std::is_same_v<T, Tarray>) {
+    if constexpr (units::is_quantity_v<T>) {
+        static_assert(units::is_same_dimension<T, Tarray>);
+
+        std::transform(a2.cbegin(),
+                       a2.cend(),
+                       a1.begin() + signed_size_t(N1),
+                       [](auto x) { return units::quantity_cast<T>(x); });
+    } else if constexpr (std::is_same_v<T, Tarray>) {
         std::copy(a2.cbegin(), a2.cend(), a1.begin() + signed_size_t(N1));
     } else if constexpr (std::is_convertible_v<T, Tarray>) {
         std::transform(a2.cbegin(),
@@ -79,7 +102,14 @@ auto concatenate(const Array &a1, std::vector<T> &&a2) {
 
     a2.reserve(a1.size() + a2.size());
 
-    if constexpr (std::is_same_v<T, Tarray>) {
+    if constexpr (units::is_quantity_v<T>) {
+        static_assert(units::is_same_dimension<T, Tarray>);
+
+        std::transform(a1.cbegin(),
+                       a1.cend(),
+                       std::inserter(a2, a2.begin()),
+                       [](auto x) { return units::quantity_cast<T>(x); });
+    } else if constexpr (std::is_same_v<T, Tarray>) {
         a2.insert(a2.begin(), a1.cbegin(), a1.cend());
     } else if constexpr (std::is_convertible_v<T, Tarray>) {
         std::transform(a1.cbegin(),
--- scicpp/core/manips.t.cpp
+++ scicpp/core/manips.t.cpp
@@ -15,22 +15,30 @@ TEST_CASE("concatenate") {
         REQUIRE(almost_equal(res, {1.0, 2.0, 3.0, 4.0, 5.0, 6.0}));
     }
 
-    // SECTION("std::array<quantity> / std::array<quantity>") {
-    //     // TODO
-
-    //     using namespace units::literals;
-
-    //     const auto a1 = std::array{1_m, 2_m, 3_m};
-    //     const auto a2 = std::array{4_mm, 5_mm, 6_mm};
-    //     const auto res = concatenate(a1, a2);
-    //     REQUIRE(almost_equal(res, {1.0, 2.0, 3.0, 4.0, 5.0, 6.0}));
-    // }
+    SECTION("std::array<quantity> / std::array<quantity>") {
+        using namespace units::literals;
+        const auto a1 = std::array{1_m, 2_m, 3_m};
+        const auto a2 = std::array{4_mm, 5_mm, 6_mm};
+        const auto res = concatenate(a1, a2);
+        // print(res);
+        REQUIRE(almost_equal(res, {1_m, 2_m, 3_m, 4_mm, 5_mm, 6_mm}));
+    }
 
     SECTION("std::array / std::vector") {
         const auto a1 = std::array{1.0, 2.0, 3.0};
         const auto a2 = std::vector{4, 5, 6, 7, 8};
         const auto res = concatenate(a1, a2);
         REQUIRE(almost_equal(res, {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0}));
+    }
+
+    SECTION("std::array<quantity> / std::vector<quantity>") {
+        using namespace units::literals;
+        const auto a1 = std::array{1_g, 2_g, 3_g};
+        const auto a2 = std::vector{4_kg, 5_kg, 6_kg, 7_kg, 8_kg};
+        const auto res = concatenate(a1, a2);
+        // print(res);
+        REQUIRE(
+            almost_equal(res, {1_g, 2_g, 3_g, 4_kg, 5_kg, 6_kg, 7_kg, 8_kg}));
     }
 
     SECTION("std::vector / std::array") {
@@ -61,6 +69,15 @@ TEST_CASE("concatenate") {
         REQUIRE(almost_equal(res, {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0}));
     }
 
+    SECTION("std::vector&& / std::array quantities") {
+        using namespace units::literals;
+        auto a1 = std::vector{1_mA, 2_mA, 3_mA};
+        const auto a2 = std::array{4_uA, 5_uA, 6_uA, 7_uA, 8_uA};
+        const auto res = concatenate(std::move(a1), a2);
+        REQUIRE(almost_equal(res,
+                             {1_mA, 2_mA, 3_mA, 4_uA, 5_uA, 6_uA, 7_uA, 8_uA}));
+    }
+
     SECTION("std::vector / std::vector&& same type") {
         const auto a1 = std::vector{1, 2, 3};
         auto a2 = std::vector{4, 5, 6, 7, 8};
@@ -77,6 +94,16 @@ TEST_CASE("concatenate") {
         // print(res);
         // print(res.size());
         REQUIRE(array_equal(res, {1, 2, 3, 4, 5, 6, 7, 8}));
+    }
+
+    SECTION("std::vector / std::vector&& quantity") {
+        using namespace units::literals;
+        const auto a1 = std::vector{1_hPa, 2_hPa, 3_hPa};
+        auto a2 = std::vector{4_Pa, 5_Pa, 6_Pa, 7_Pa, 8_Pa};
+        const auto res = concatenate(a1, std::move(a2));
+        // print(res);
+        REQUIRE(almost_equal(
+            res, {1_hPa, 2_hPa, 3_hPa, 4_Pa, 5_Pa, 6_Pa, 7_Pa, 8_Pa}));
     }
 
     SECTION("Operator |") {
