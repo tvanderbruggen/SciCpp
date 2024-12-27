5ebf8053ec283498f659ab837b9853b9b7f8f682
Thomas Vanderbruggen <th.vanderbruggen@gmail.com>
2024-12-27 10:52:55 +0100

    Use class enums

--- examples/white-noise.cpp
+++ examples/white-noise.cpp
@@ -15,7 +15,7 @@ int main() {
            sci::stats::std(noise));
 
     const auto [f, psd] = sci::signal::Spectrum{}
-                              .window(sci::signal::windows::Flattop, 2000)
+                              .window(sci::signal::windows::Window::Flattop, 2000)
                               .welch(noise);
 
     // The expected PSD for a white noise is 2 * sigma ^ 2
