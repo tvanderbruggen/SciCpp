d742f45c8040d17b69e0c53fd829729cd6c29f1b
Thomas Vanderbruggen <th.vanderbruggen@gmail.com>
2025-09-28 21:23:25 +0200

    CODATA 2022 for constants

--- scicpp/core/constants.hpp
+++ scicpp/core/constants.hpp
@@ -11,6 +11,8 @@
 #include "scicpp/core/units/quantity.hpp"
 #include "scicpp/core/units/units.hpp"
 
+#include <concepts>
+
 #endif // SCICPP_HAS_UNITS
 
 namespace scicpp {
@@ -19,17 +21,17 @@ namespace scicpp {
 // Mathematical constants
 //---------------------------------------------------------------------------------
 
-template <typename T>
+template <std::floating_point T>
 constexpr T pi = T(3.1415926535897932384626433832795029);
 
-template <typename T>
+template <std::floating_point T>
 constexpr T e = T(2.7182818284590452353602874713526625);
 
-template <typename T>
+template <std::floating_point T>
 constexpr T euler_gamma = T(0.5772156649015328606065120900824024310421);
 
 // Apéry's constant zeta(3)
-template <typename T>
+template <std::floating_point T>
 constexpr T apery_cst = T(1.20205690315959428539);
 
 //---------------------------------------------------------------------------------
@@ -40,38 +42,95 @@ constexpr T apery_cst = T(1.20205690315959428539);
 
 #if SCICPP_HAS_UNITS
 
-template <typename T>
+template <std::floating_point T>
+using newton_grav_cst_qty = units::quantity_divide<
+    units::volume<T>,
+    units::quantity_multiply<
+        units::mass<T>,
+        units::quantity_multiply<units::time<T>, units::time<T>>>>;
+
+struct codata2018 {
+    template <std::floating_point T>
+    static constexpr auto m_e() noexcept {
+        return units::mass<T>(T(9.1093837015E-31));
+    }
+
+    template <std::floating_point T>
+    static constexpr auto m_p() noexcept {
+        return units::mass<T>(T(1.67262192369E-27));
+    }
+
+    template <std::floating_point T>
+    static constexpr auto alpha() noexcept {
+        return T(7.2973525693E-3);
+    }
+
+    template <std::floating_point T>
+    static constexpr auto G() noexcept {
+        return newton_grav_cst_qty<T>(6.67430E-11);
+    }
+};
+
+struct codata2022 {
+    template <std::floating_point T>
+    static constexpr auto m_e() noexcept {
+        return units::mass<T>(T(9.1093837139E-31));
+    }
+
+    template <std::floating_point T>
+    static constexpr auto m_p() noexcept {
+        return units::mass<T>(T(1.67262192595E-27));
+    }
+
+    template <std::floating_point T>
+    static constexpr auto alpha() noexcept {
+        return T(7.2973525643E-3);
+    }
+
+    template <std::floating_point T>
+    static constexpr auto G() noexcept {
+        return newton_grav_cst_qty<T>(6.67430E-11);
+    }
+};
+
+template <std::floating_point T, class Codata>
 struct physical_constants {
-    static_assert(std::is_floating_point_v<T>);
-
-    // Speed of light in vacuum
-    static constexpr auto c = units::speed<T>(299792458);
-
-    // Newtonian constant of gravitation
-    using newton_grav_cst_qty = units::quantity_divide<
-        units::volume<T>,
-        units::quantity_multiply<
-            units::mass<T>,
-            units::quantity_multiply<units::time<T>, units::time<T>>>>;
-    static constexpr auto G = newton_grav_cst_qty(6.67430E-11);
-
-    // Planck constant
     using planck_cst_qty =
         units::quantity_multiply<units::energy<T>, units::time<T>>;
+
+    using boltzmann_cst_qty =
+        units::quantity_divide<units::energy<T>, units::temperature<T>>;
+
+    // --------- Exact constants
+
+    // Speed of light in vacuum (exact)
+    static constexpr auto c = units::speed<T>(299792458);
+
+    // Elementary charge (exact)
+    static constexpr auto e = units::electric_charge<T>(T(1.602176634E-19));
+
+    // Planck constant (exact)
     static constexpr auto h = planck_cst_qty(T(6.62607015E-34));
     static constexpr auto hbar = h / (T{2} * pi<T>);
 
-    // Elementary charge
-    static constexpr auto e = units::electric_charge<T>(T(1.602176634E-19));
+    // Boltzmann constant (exact)
+    static constexpr auto k = boltzmann_cst_qty(1.380649E-23);
+
+    // --------- Other constants
+
+    // Newtonian constant of gravitation
+    static constexpr auto G = Codata::template G<T>();
 
     // Electron mass
-    static constexpr auto m_e = units::mass<T>(T(9.1093837015E-31));
+    static constexpr auto m_e = Codata::template m_e<T>();
 
     // Proton mass
-    static constexpr auto m_p = units::mass<T>(T(1.67262192369E-27));
+    static constexpr auto m_p = Codata::template m_p<T>();
 
     // Fine-structure constant
-    static constexpr auto alpha = T(7.2973525693E-3);
+    static constexpr auto alpha = Codata::template alpha<T>();
+
+    // --------- Derived constants
 
     // Vacuum magnetic permeability
     static constexpr auto mu0 = T{2} * alpha * h / (e * e * c);
@@ -89,26 +148,21 @@ struct physical_constants {
     static constexpr auto Phi0 = T{1} / K_J;
 
     // Bohr magneton
-    static constexpr auto muB = T{0.5} * e * hbar / m_e;
+    static constexpr auto muB = static_cast<T>(0.5L) * e * hbar / m_e;
 
     // Nuclear magneton
-    static constexpr auto muN = T{0.5} * e * hbar / m_p;
+    static constexpr auto muN = static_cast<T>(0.5L) * e * hbar / m_p;
 
     // Rydberg constant
-    static constexpr auto Rinf = T{0.5} * alpha * alpha * m_e * c / h;
+    static constexpr auto Rinf = static_cast<T>(0.5L) * alpha * alpha * m_e * c / h;
 
     // Bohr radius
     static constexpr auto a0 = hbar / (alpha * m_e * c);
-
-    // Boltzmann constant
-    using boltzmann_cst_qty =
-        units::quantity_divide<units::energy<T>, units::temperature<T>>;
-    static constexpr auto k = boltzmann_cst_qty(1.380649E-23);
 }; // struct physical_constants
 
-using phys_cst_f = physical_constants<float>;
-using phys_cst = physical_constants<double>;
-using phys_cst_l = physical_constants<long double>;
+using phys_cst_f = physical_constants<float, codata2022>;
+using phys_cst = physical_constants<double, codata2022>;
+using phys_cst_l = physical_constants<long double, codata2022>;
 
 #endif // SCICPP_HAS_UNITS
 
--- scicpp/core/constants.t.cpp
+++ scicpp/core/constants.t.cpp
@@ -10,22 +10,28 @@ TEST_CASE("Physical constants") {
     REQUIRE(almost_equal(phys_cst::c, 299792.458_km_per_s));
     REQUIRE(
         almost_equal<2>(1E34 * phys_cst::hbar, 1.05457181764615634794_J * 1_s));
+    // printf("%.24Lf\n", phys_cst_l::mu0.value());
     REQUIRE(almost_equal<2>(1E6 * phys_cst::mu0,
-                            1.256637062123837634_N / 1_A / 1_A));
+                            1.256637061262814882_N / 1_A / 1_A));
+    // printf("%.30Lf\n", phys_cst_l::epsilon0.value());
     REQUIRE(
-        almost_equal<2>(1E12 * phys_cst::epsilon0, 8.854187812773345_F / 1_m));
+        almost_equal<2>(1E12 * phys_cst::epsilon0, 8.854187818840058860_F / 1_m));
+    // printf("%.30Lf\n", phys_cst_l::K_J.value());
     REQUIRE(almost_equal<2>(phys_cst::K_J, 483597.848416983625E9_Hz / 1_V));
+    // printf("%.30Lf\n", phys_cst_l::R_K.value());
     REQUIRE(almost_equal<2>(phys_cst::R_K, 25812.807459304513031_Ohm));
+    // printf("%.30Lf\n", phys_cst_l::Phi0.value());
     REQUIRE(almost_equal<2>(1E15 * phys_cst::Phi0, 2.067833848461929414_Wb));
+    // printf("%.30Le\n", phys_cst_l::muB.value());
     REQUIRE(
-        almost_equal<2>(1E24 * phys_cst::muB, 9.274010078362163512_J / 1_T));
+        almost_equal<2>(1E24 * phys_cst::muB, 9.274010065738068515475452810387_J / 1_T));
+    // printf("%.30Le\n", phys_cst_l::muN.value());
     REQUIRE(
-        almost_equal<2>(1E27 * phys_cst::muN, 5.050783746096374927_J / 1_T));
-    // printf("%.18Lf\n", phys_cst_l::Rinf.value());
-    // NB: Discrepency with CODATA numerical value (10973731.568160)
-    REQUIRE(almost_equal<2>(phys_cst::Rinf, 10973731.568138644099235535 / 1_m));
-    // printf("%.18f\n", 1E11 * phys_cst::a0.value());
-    REQUIRE(almost_equal<2>(1E11 * phys_cst::a0, 5.291772109044924122_m));
+        almost_equal<2>(1E27 * phys_cst::muN, 5.050783739271897144306250269724_J / 1_T));
+    // printf("%.30Le\n", phys_cst_l::Rinf.value());
+    REQUIRE(almost_equal<2>(phys_cst::Rinf, 10973731.56803849312655074754730 / 1_m));
+    // printf("%.30Le\n", phys_cst_l::a0.value());
+    REQUIRE(almost_equal<2>(1E11 * phys_cst::a0, 5.291772105467402030742718729114_m));
 }
 
 } // namespace scicpp
